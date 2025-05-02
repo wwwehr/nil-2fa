6a786a18a7ca8959eeec19f15952e8bb0854a8dc
Nicholas Wehr <github@biochx.com>
2025-05-02 15:41:45 -0700

    adding deployment stuff

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/:path*", // Proxy to your backend
+        destination: `${process.env.API_URL}/:path*`,
       },
     ];
   },
